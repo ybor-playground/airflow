c1434c3140dfdfcb4f7508cf36febe604956c5ea
yogesh-keshetty_naxgroup <yogesh.keshetty@naxgrp.com>
2024-11-04 22:59:31 -0800

    Changing Hello world DAG

--- dags/examples/hello.py
+++ dags/examples/hello.py
@@ -1,18 +1,39 @@
-from datetime import datetime
-
+from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.decorators import task
-from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
 
-# A DAG represents a workflow, a collection of tasks
-with DAG(dag_id="demo", start_date=datetime(2024, 11, 1), schedule="0 0 * * *") as dag:
+# Define default arguments
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 11, 4),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
 
-    # Tasks are represented as operators
-    hello = BashOperator(task_id="hello", bash_command="echo hello")
+# Instantiate the DAG
+dag = DAG(
+    'hello_world',
+    default_args=default_args,
+    description='A simple Hello World DAG',
+    schedule_interval=timedelta(days=1),
+)
 
-    @task()
-    def airflow():
-        print("airflow")
+# Define the Python function to be executed
+def print_hello():
+    return 'Hello World from Airflow!'
 
-    # Set dependencies between tasks
-    hello >> airflow()
+# Create tasks
+start_task = EmptyOperator(task_id='start_task', dag=dag)
+hello_task = PythonOperator(
+    task_id='hello_task',
+    python_callable=print_hello,
+    dag=dag,
+)
+end_task = EmptyOperator(task_id='end_task', dag=dag)
+
+# Set task dependencies
+start_task >> hello_task >> end_task
