bc07d6a1e22091a1a334ff9716fe516c91e92b8b
Gurinder <gurinder@ybor.ai>
2025-10-13 09:20:06 -0700

    Update knowledge_graph_pipeline.py

--- src/dags/demo_dags/knowledge_graph_pipeline.py
+++ src/dags/demo_dags/knowledge_graph_pipeline.py
@@ -15,7 +15,7 @@ class Constants:
 
     # CI/CD Configurations
     DOCKER_IMAGE_PREFIX = f"p6m.jfrog.io/{ORG}-{VENTURE}-docker/applications"
-    KG_DRIVER_DOCKER_IMAGE = f"{DOCKER_IMAGE_PREFIX}/ybor-graph-query-adapter-server:main"
+    KG_DRIVER_DOCKER_IMAGE = f"{DOCKER_IMAGE_PREFIX}/ybor-graph-query-adapter-server:latest"
 
 
     PULL_SECRET = "dockerconfig"  # "regcred"
